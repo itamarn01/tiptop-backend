1f591cb895a5f689c807556602c5afcb717bb5ca
itamarn01 <104838462+itamarn01@users.noreply.github.com>
2026-02-16 10:11:41 +0200

    Change MongoDB connection string to live database

--- src/DB/index.js
+++ src/DB/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const dbConnectAtlas = "mongodb+srv://tiptoptherapyapp:341IcvrwkjvHSTWh@tiptop.izmnu.mongodb.net/test";
+const dbConnectAtlas = "mongodb+srv://tiptoptherapyapp:341IcvrwkjvHSTWh@tiptop.izmnu.mongodb.net/live";
 mongoose.connect(dbConnectAtlas, { /* useNewUrlParser: true, useUnifiedTopology: true */ }).then(() => {
     console.log("mongo connected");
 })
