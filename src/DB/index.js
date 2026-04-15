10f93a3a424bc1fb44cf57bab5931c6c552bdd11
itamarn01 <104838462+itamarn01@users.noreply.github.com>
2026-04-16 01:19:46 +0300

    Update MongoDB connection string in index.js

--- src/DB/index.js
+++ src/DB/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
-//const dbConnectAtlas = "mongodb+srv://tiptoptherapyapp:341IcvrwkjvHSTWh@tiptop.izmnu.mongodb.net/live";
+//const dbConnectAtlas = "mongodb+srv://tiptoptherapyapp:341IcvrwkjvHSTWh@tiptop.izmnu.mongodb.net/test";
 const dbConnectAtlas = "mongodb+srv://tiptoptherapyapp_db_user:Tiptop1234@tiptop.b4ik2vh.mongodb.net/";
+
 mongoose.connect(dbConnectAtlas, { /* useNewUrlParser: true, useUnifiedTopology: true */ }).then(() => {
     console.log("mongo connected");
 })
